0cbca2cc03963d0e5229325401d80fa68e726b49
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2023-08-25 01:17:32 -0300

    code wrap in a autoexecutable function

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,8 @@
 import { game } from "./modules/gameEngine.js";
 
-//Funcion que da un numero random
 
-
-
-
-
-const $btnStart =  document.getElementById("startGame");
-
-$btnStart.addEventListener(`click`, game);
+(()=>{
+    const $btnStart =  document.getElementById("startGame");
+    
+    $btnStart.addEventListener(`click`, game);
+})();
