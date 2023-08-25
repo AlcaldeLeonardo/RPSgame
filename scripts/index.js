5f748a879d08fe439da46eec57129e67c99c497b
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2023-08-24 23:32:12 -0300

    functions for computer selection added
    jetComputer Choice bases its return on getRandomNumber

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,73 @@
 console.log("Hola! soy la consola")
+
+
+//FUNCIONES
+
+    //Funcion que da un numero random
+function getRandomNumber() {
+    return Math.floor(Math.random()*3)
+}
+
+
+    //funcion que toma la desicion de la computadora
+function getComputerChoice(){
+    let choice = getRandomNumber();
+
+    if(choice == 0) return `Rock`
+    else if(choice == 1) return `Paper`
+    else return `Scissors`
+}
+
+
+    //funcion que lanza el round
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
+
+    
+    while(playerSelection === computerSelection){
+        computerSelection = getComputerChoice().toLocaleLowerCase();
+        console.log(`la computadora eligio: ${computerSelection}`)
+        playerSelection = prompt("Tied game, pick again").toLocaleLowerCase();
+    }
+
+    if (playerSelection == `rock` && computerSelection == `scissors`) {
+        console.log(`You Win!! ${playerSelection} beat ${computerSelection}`)
+        return `win`
+    } else if (playerSelection == `rock` && computerSelection == `paper`) {
+        console.log(`You Lose!! ${playerSelection} beat ${computerSelection}`)
+        return `lose`
+    } else if (playerSelection == `paper` && computerSelection == `rock`) {
+        console.log(`You Win!! ${playerSelection} beat ${computerSelection}`)
+        return `win`
+    } else if (playerSelection == `paper` && computerSelection == `scissors`) {
+        console.log(`You Lose!! ${playerSelection} beat ${computerSelection}`)
+        return `lose`
+    } else if (playerSelection == `scissors` && computerSelection == `paper`) {
+        console.log(`You Win!! ${playerSelection} beat ${computerSelection}`)
+        return `win`
+    } else if (playerSelection == `scissors` && computerSelection == `rock`) {
+        console.log(`You Lose!! ${playerSelection} beat ${computerSelection}`)
+        return `lose`
+    }
+}
+
+function validatePlayerChoice(playerSelection){
+    return (playerSelection != `rock` && playerSelection != `paper` && playerSelection != `scissors`);
+}
+
+
+
+
+//PROGRAMA PRINCIPAL
+function game(){
+
+}
+
+let playerSelection;
+
+do{
+    playerSelection = prompt("elige opcion valida: ")
+    console.log(playerSelection);
+}while(validatePlayerChoice(playerSelection));
+
+playRound(playerSelection,getComputerChoice().toLowerCase())
